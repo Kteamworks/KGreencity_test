b9b182b64e287f4e3c8018d8348303331303d00e
kteamwork <searchgeetika111@gmail.com>
2017-04-03 12:04:46 +0530

    test commit

--- greencity_test/interface/main/left_nav.php
+++ greencity_test/interface/main/left_nav.php
@@ -1302,8 +1302,7 @@ if ($GLOBALS['athletic_team']) {
 	  <?php } ?>
 	  <?php genMiscLink('RTop','fin','0',xl('My Patients'),'main/finder/p_dynamic_finder.php'); ?>
 	  <?php if($newcrop_user_role['newcrop_user_role']!='erxdoctor' ||$_SESSION['authUser']=='DR. NIRMALA.B.M') {?>
-	  <?php genMiscLink('RTop','fin','0',xl('InPatients'),'main/finder/p_dynamic_finder_ip.php'); 
-	  //genMiscLink('RTop','fin','0',xl('Todays Patients'),'main/finder/p_dynamic_finder_tdap.php'); ?>
+	  <?php genMiscLink('RTop','fin','0',xl('InPatients'),'main/finder/p_dynamic_finder_ip.php'); ?>
 	   <?php }?>
       <?php genTreeLink('RTop','new',($GLOBALS['full_new_patient_form'] ? xl('New/Search') : xl('New'))); ?>
       <?php genTreeLink('RTop','dem',xl('Summary')); ?>
